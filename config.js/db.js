90010f97219c0b18799269d2da19a93f96e4977c
Ashish Saini <ashu78021@gmail.com>
2024-03-06 22:34:53 +0530

    first commit

---
+++ config.js/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+async function main(){
+    try {
+        await mongoose.connect(process.env.URL);
+    } catch (err) {
+        console.log(err.bgCyan.white);
+    }
+}
+
+module.exports = main;
