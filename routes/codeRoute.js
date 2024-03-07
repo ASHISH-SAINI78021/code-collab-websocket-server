ef43837cf1abdc54a7ddfbdbc96c32676975321f
Ashish Saini <ashu78021@gmail.com>
2024-03-07 07:53:50 +0530

    fisjf

--- routes/codeRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {codeController} = require("../controllers/codeController.js");
-
-router.post("/main/code" , codeController.saveCode);
-
-
-module.exports = router;
